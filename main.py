04c804e19654464f9ac39259291c31f363800727
mardeanm <mardeanmontane@gmail.com>
2023-02-22 09:30:32 -0800

    error handling

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ data=pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet={row.letter:row.code for (index,row) in data.iterrows()}
 
 
-def convert_name(name):
+def convert_name():
+
     global alphabet
-    name=list(name.upper())
-    name=[alphabet[letter] for letter in name ]
-    for n in name:
-        print(n)
-convert_name(input("What is your name? "))
+    name = input("What is your name? ")
+    final_name=list(name.upper())
+    try:
+        final_name=[alphabet[letter] for letter in final_name ]
+    except KeyError:
+        print("Sorry only letters, please")
+        convert_name()
+    else:
+        print(final_name)
+
+convert_name()
